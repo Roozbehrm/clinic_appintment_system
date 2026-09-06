0f4e562e2897278b7bdb439e5314345e49c02a42
Roozbehrm <152732283+Roozbehrm@users.noreply.github.com>
2026-09-07 00:47:12 +0330

    Register Patient admin and add PatientForm

--- patients/admin.py
+++ patients/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Patient
 
-# Register your models here.
+
+@admin.register(Patient)
+class PatientAdmin(admin.ModelAdmin):
+    list_display = ["profile", "birth_date"]
+    search_fields = ["profile__full_name"]
