26790a915cfaba7a5ba0c405c24a40c727af631f
Roozbehrm <152732283+Roozbehrm@users.noreply.github.com>
2026-09-08 19:39:17 +0330

    Add HomeView route at root URL

--- clinic_appointment_system/urls.py
+++ clinic_appointment_system/urls.py
@@ -17,9 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path , include
 
+from doctors.views import HomeView
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("social-auth/", include("allauth.urls")),
+    path("", HomeView.as_view(), name="home"),
     path("", include("accounts.urls")),
     path("doctors/", include("doctors.urls")),
     path("patients/", include("patients.urls")),
