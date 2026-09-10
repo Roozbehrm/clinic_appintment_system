73e96925c790f05bf36ef68434d6823821d4a493
ZahraRahimpour95 <faezehr.95@gmail.com>
2026-09-10 12:45:52 +0330

    payments/models.py updated

--- payments/models.py
+++ payments/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Wallet(models.Model):
-    patient_id = models.OneToOneField(to='patient' , related_name = 'wallet', on_delete=models.CASCADE, null =False)
+    patient_id = models.OneToOneField(to='patients.Patient' , related_name = 'wallet', on_delete=models.CASCADE, null =False)
     balance = models.DecimalField(max_digits=12, decimal_places=0, default=0,
                                   verbose_name = 'موجودی'
                                   )
@@ -15,8 +15,8 @@ class Wallet(models.Model):
 class Transaction(models.Model):
     STATUS_CHOICES = [('success','success'), ('failed','failed'), ('pending','pending'),]
     TYPE_CHOICES = [('deposit','deposit'),('payment','payment'),('refund','refund'),]
-    wallet_id = models.ForeignKey(to='Wallet', related_name='transaction', on_delete=models.CASCADE, null =False)
-    appointment_id = models.ForeignKey(to='appointment', related_name='transaction', on_delete=models.CASCADE, null = True, blank = True)
+    wallet_id = models.ForeignKey(to='payments.Wallet', related_name='transaction', on_delete=models.CASCADE, null =False)
+    appointment_id = models.ForeignKey(to='appointments.Appointment', related_name='transaction', on_delete=models.CASCADE, null = True, blank = True)
     amount = models.DecimalField(max_digits=12, decimal_places=0,
                                   verbose_name ='مقدار')
     type = models.CharField(choices = TYPE_CHOICES,  max_length = 50,
