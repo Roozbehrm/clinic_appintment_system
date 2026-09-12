ee4036aa1120c04d37d7211e58dbe2686635b095
MahyarDev01 <mahyar.dev01@gmail.com>
2026-09-12 10:05:27 +0330

    change ROOT_URLCONF by Mahyar

--- clinic_appointment_system/settings/base.py
+++ clinic_appointment_system/settings/base.py
@@ -40,7 +40,7 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = "config.urls"
+ROOT_URLCONF = "clinic_appointment_system.urls"
 
 TEMPLATES = [
     {
