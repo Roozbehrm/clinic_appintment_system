88bbc215ee4d3b1681930e5fa880cb1e041ef65a
Roozbehrm <152732283+Roozbehrm@users.noreply.github.com>
2026-09-14 04:18:03 +0330

    Fix patient model problems

--- patients/models.py
+++ patients/models.py
@@ -1,16 +1,16 @@
 from django.db import models
 from accounts.models import Profile
 
-class Patient(models.Model):
 
-    profile = models.OneToOneField(Profile, on_delete=models.CASCADE, verbose_name='پروفایل بیمار', related_name='patient_profile')
-    birth_date = models.DateField(null=True, blank=True, verbose_name='تاریخ تولد')
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name='تاریخ ایجاد')
-    updated_at = models.DateTimeField(auto_now=True, verbose_name='تاریخ به‌روزرسانی')
+class Patient(models.Model):
+    profile = models.OneToOneField(Profile, on_delete=models.CASCADE, related_name="patient")
+    birth_date = models.DateField("تاریخ تولد", null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
-        verbose_name = 'بیمار'
-        verbose_name_plural = 'بیماران'
+        verbose_name = "بیمار"
+        verbose_name_plural = "بیماران"
 
     def __str__(self):
-        return self.profile.full_name
+        return self.profile.full_name or self.profile.user.phone_number
